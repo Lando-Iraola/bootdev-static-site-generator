3e4c72f423f6326c447f6469a43fc3c41f986344
Lando <lando_vargas@outlook.com>
2026-01-30 17:07:24 -0300

    CH4 L3

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from unittest import case
+
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node, TextNode, TextType
+from htmlnode import ParentNode
 
 
 class BlockType(Enum):
@@ -46,3 +51,97 @@ def block_to_block_type(block):
             i += 1
         return BlockType.OLIST
     return BlockType.PARAGRAPH
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        children.append(block_to_html_node(block))
+
+    return ParentNode("div", children)
+
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    match block_type:
+        case BlockType.OLIST:
+            return olist_to_html_node(block)
+        case BlockType.ULIST:
+            return ulist_to_html_node(block)
+        case BlockType.PARAGRAPH:
+            return paragraph_to_html_node(block)
+        case BlockType.QUOTE:
+            return quote_to_html_node(block)
+        case BlockType.CODE:
+            return code_to_html_node(block)
+        case BlockType.HEADING:
+            return heading_to_html_node(block)
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph_text = " ".join(lines)
+    children = text_to_children(paragraph_text)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    heading_level = 0
+    for c in block:
+        if c == "#":
+            heading_level += 1
+        else:
+            break
+    text = block[heading_level + 1:]
+    children = text_to_children(text)
+    return ParentNode(f"h{heading_level}", children)
+
+
+def code_to_html_node(block):
+    lines = block.split("\n")
+    inner_lines = lines[1:-1]
+    text = "\n".join(inner_lines)
+    if not text.endswith("\n"):
+        text += "\n"
+    child = text_node_to_html_node(TextNode(text, TextType.TEXT))
+    holder = ParentNode("code", [child])
+    return ParentNode("pre", [holder])
+
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    list_items = []
+    for item in items:
+        children = text_to_children(item.split(".", 1)[1])
+        list_items.append(ParentNode("li", children))
+    return ParentNode("ol", list_items)
+
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    list_items = []
+    for item in items:
+        children = text_to_children(item[2:])
+        list_items.append(ParentNode("li", children))
+    return ParentNode("ul", list_items)
+
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    cleaned_lines = []
+    for line in lines:
+        cleaned = line.lstrip(">").strip()
+        cleaned_lines.append(cleaned)
+    content = " ".join(cleaned_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
+
+def text_to_children(text):
+    nodes = text_to_textnodes(text)
+    html_nodes = []
+    for node in nodes:
+        html_nodes.append(text_node_to_html_node(node))
+    return html_nodes
