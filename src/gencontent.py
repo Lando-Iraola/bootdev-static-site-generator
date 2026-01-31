614f71346fd920d7ebfd75e0fa087cae462955b5
Lando <lando_vargas@outlook.com>
2026-01-30 21:28:48 -0300

    Fixed styling

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,11 +1,15 @@
 import os
+
 from markdown_blocks import markdown_to_html_node
+
+
 def extract_title(markdown):
     lines = markdown.split("\n")
     for line in lines:
         if line.startswith("# "):
             return line[2:].strip()
     raise Exception("Could not extract title")
+
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page {from_path} to {dest_path} using {template_path}")
